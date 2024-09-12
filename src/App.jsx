ebbc843317cc5839fd46be0e42595776a9470949
Milena <109882477+MilenaGarciaCosta@users.noreply.github.com>
2024-09-12 11:08:01 -0300

    components e suas respectivas rotas criadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import Nav from './components/Nav.jsx'
+import Footer from './components/Footer.jsx'
+import { Outlet } from 'react-router-dom';
+
 function App() {
   return (
     <>
+    <Nav/>
+    <Outlet/>
+    <Footer/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import Error from './router/Error.jsx'
+import Home from './router/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    //Elementos Pai
+    path: '/', element:<App/>,
+    errorElement: <Error/>,
+
+    //Elementos Filho
+    children:[
+      {path: '/', element:<Home/>},
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
