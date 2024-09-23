8030f67ea0afe19eefe920949ea7978c92496d72
Enzo <enzovdias04@gmail.com>
2024-09-23 11:43:53 -0300

    preparação do slideShow com a biblioteca swiper

--- src/router/Home.jsx
+++ src/router/Home.jsx
@@ -1,10 +1,35 @@
 import "../css/home.css"
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import { register } from "swiper/element/bundle";
+register();
+import banner1 from "../img/banner1.png";
+import banner2 from "../img/banner2.png";
+import banner3 from "../img/banner3.png";
 
 const Home = () => {
+  const data = [
+    { id: "1", image: banner1 },
+    { id: "2", image: banner2 },
+    { id: "3", image: banner3 },
+  ];
   return (
+    <>
+    <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        navigation
+      >
+        {data.map((item) => (
+          <SwiperSlide key={item.id}>
+            <img className="banner" src={item.image} alt={`Slide ${item.id}`} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     <section className="main">
+        
     </section>
+    </>
   );
 };
 
