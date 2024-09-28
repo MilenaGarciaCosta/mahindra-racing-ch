7c9b9b38791d84f7e92cc9298680d6595c1a0530
Milena <109882477+MilenaGarciaCosta@users.noreply.github.com>
2024-09-28 10:38:37 -0300

    servidor de chat rodando

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const app = require('express')()
 const server = require('http').createServer(app)
-const io = require('socket.io')(server, {cors: {origin: 'http://localhost:5173'}}) // URL do front end
+const io = require('socket.io')(server, {cors: {origin: 'http://localhost:5173'}}) // URL do front-end
+
 const PORT = 3001
 
 io.on('connection', socket => {
